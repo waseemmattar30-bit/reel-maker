46c090460c0b148ae0d894d70568cc3670926f5e
Waseem <waseemmr16@outlook.com>
2026-05-22 15:22:59 +0300

    Fix CORS allow all

--- backend/main.py
+++ backend/main.py
@@ -17,8 +17,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://reel-maker-dusky.vercel.app"],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
     expose_headers=["X-Session-ID"],
