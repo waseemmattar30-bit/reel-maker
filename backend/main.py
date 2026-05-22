5215f790953587fc3b9d8912de8591f2b8995f1e
Waseem <waseemmr16@outlook.com>
2026-05-22 14:58:27 +0300

    Fix CORS

--- backend/main.py
+++ backend/main.py
@@ -16,7 +16,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://reel-maker-dusky.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
